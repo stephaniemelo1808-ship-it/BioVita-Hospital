e1c0f74f7776490851cf75059c31d1479b4a8eb9
debz-S <deborasampaio049@gmail.com>
2026-04-22 19:58:56 -0300

    teste

--- biovita/dashboard.php
+++ biovita/dashboard.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard - MedSystem Bio Vita</title>
-    <link rel="stylesheet" href="../css/style.cs">
+    <link rel="stylesheet" href="../css/style.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    
+    */aaaaaaa/*
     <style>
         .dashboard-grid {
             display: grid;
